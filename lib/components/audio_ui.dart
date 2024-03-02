f491c803ef02b527a0ed6a64daf9a841b636734e
Sairaj <s.r.ghosarwadkar@gmail.com>
2024-03-02 12:44:28 +0530

    Assets -> Hosted

--- lib/components/audio_ui.dart
+++ lib/components/audio_ui.dart
@@ -9,34 +9,30 @@ AudioPlayer player = AudioPlayer();
 bool repeat = false;
 
 class AudioUI extends StatefulWidget {
-
   Duration? initial;
   Duration? end;
   bool _playing = false;
   AudioUI({this.initial, this.end});
+  Future? setup;
 
   @override
   State<AudioUI> createState() => _AudioUIState();
 }
 
 class _AudioUIState extends State<AudioUI> {
-  void initState(){
+  void initState() {
     super.initState();
-    player.setAsset('assets/sound/bhanjan.mp3');
-    player.seek(widget.initial!);
-    player.setClip(start: widget.initial! , end : widget.end!);
+    widget.setup = setup(widget);
   }
 
   @override
-  void dispose(){
+  void dispose() {
     player.stop();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
@@ -47,82 +43,99 @@ class _AudioUIState extends State<AudioUI> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Container(
-              height: 7/10*(screenHeight),
-              width : 9.0/10*(screenWidth),
+              height: 7 / 10 * (screenHeight),
+              width: 9.0 / 10 * (screenWidth),
               child: Image.asset('assets/shreenath.JPEG'),
             ),
-
             Container(
-              height: 1/10*(screenHeight),
-              width: 9.0/10*screenWidth,
+              height: 1 / 10 * (screenHeight),
+              width: 9.0 / 10 * screenWidth,
               child: StreamBuilder<Duration>(
                 stream: player.positionStream,
                 builder: (context, snapshot) {
                   final progress = (snapshot.data) ?? widget.initial;
                   final total = widget.end! - widget.initial!;
 
-                  if(progress == total && repeat){
-                      restart(player, widget.initial!, widget.end!);
+                  if (progress == total && repeat) {
+                    restart(player, widget.initial!, widget.end!);
                   }
 
-                   return ProgressBar(
-                      progress: progress!,
-                      total: total,
-                      onSeek: (duration) {
-                        player.seek(duration);
-                      },
-                    );
-                  },
-                ),
+                  return ProgressBar(
+                    progress: progress!,
+                    total: total,
+                    onSeek: (duration) async {
+                      await player.seek(duration);
+                    },
+                  );
+                },
+              ),
             ),
-
-              Container(
-                height: 1/10*(screenHeight),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            icon: CustomIcon(icon : Icons.fast_rewind),
-                            onPressed:  () {fastRewind(player, widget.initial!, widget.end!);},
-                        ),
-                        IconButton(
-                          icon: CustomIcon(icon: Icons.stop),
+            Container(
+              height: 1 / 10 * (screenHeight),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: CustomIcon(icon: Icons.fast_rewind),
+                        onPressed: () {
+                          fastRewind(player, widget.initial!, widget.end!);
+                        },
+                      ),
+                      IconButton(
+                        icon: CustomIcon(icon: Icons.stop),
+                        onPressed: () {
+                          setState(() {
+                            widget._playing = false;
+                            stop(player, widget.initial!, widget.end!);
+                          });
+                        },
+                      ),
+                      FutureBuilder(
+                          future: widget.setup,
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.done)
+                              return IconButton(
+                                  icon: widget._playing
+                                      ? CustomIcon(icon: Icons.pause)
+                                      : CustomIcon(icon: Icons.play_arrow),
+                                  onPressed: () async {
+                                    widget._playing
+                                        ? await player.pause()
+                                        : player.play();
+                                    setState(() {
+                                      widget._playing = !widget._playing;
+                                    });
+                                  });
+                            else {
+                              return CircularProgressIndicator(color : Colors.blue, strokeWidth: 1.0);
+                            }
+                          }),
+                      IconButton(
+                          icon: repeat
+                              ? CustomIcon(icon: Icons.repeat_on_sharp)
+                              : CustomIcon(icon: Icons.repeat),
                           onPressed: () {
                             setState(() {
-                              widget._playing = false;
-                              stop(player, widget.initial!, widget.end!);
+                              repeat = !repeat;
                             });
-                          },
-                        ),
-
-                        IconButton(
-                            icon: widget._playing
-                                ? CustomIcon(icon: Icons.pause)
-                                : CustomIcon(icon: Icons.play_arrow),
-                            onPressed: () {
-                              widget._playing
-                                  ? player.pause()
-                                  : player.play();
-                              setState(() {
-                                widget._playing = !widget._playing;
-                              });
-                            }),
-                        IconButton(icon: repeat ? CustomIcon(icon : Icons.repeat_on_sharp) : CustomIcon(icon : Icons.repeat), onPressed: (){
-                          setState(() {
-                            repeat = !repeat;
-                          });
-                        }),
-                        IconButton(icon: CustomIcon(icon: Icons.fast_forward), onPressed: (){fastForward(player, widget.end!, widget.initial!);})
-                      ],
-                    ),
-                  ],
-                ),
+                          }),
+                      IconButton(
+                          icon: CustomIcon(icon: Icons.fast_forward),
+                          onPressed: () {
+                            fastForward(player, widget.end!, widget.initial!);
+                          })
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
+
--- lib/components/functions.dart
+++ lib/components/functions.dart
@@ -1,27 +1,40 @@
 import 'package:just_audio/just_audio.dart';
 import 'package:flutter/material.dart';
+import 'audio_ui.dart';
+import 'bhajan_time_map.dart';
 
-void fastRewind (AudioPlayer player, Duration initial, Duration end){
+
+
+void fastRewind (AudioPlayer player, Duration initial, Duration end) async {
   Duration current = player.position;
   Duration seek = Duration(seconds: 10);
-  current - seek < Duration.zero ? player.seek(Duration.zero) :  player.seek(current - seek);
+  current - seek < Duration.zero ? await player.seek(Duration.zero) : await player.seek(current - seek);
 }
 
-void fastForward (AudioPlayer player, Duration end, Duration initial){
+void fastForward (AudioPlayer player, Duration end, Duration initial) async {
   Duration current = player!.position;
   Duration seek = Duration(seconds: 10);
-  current + seek  < end - initial  ? player.seek(current + seek) : player.seek(end - initial);
+  current + seek  < end - initial  ? await player.seek(current + seek) : await player.seek(end - initial);
 }
 
-void stop (AudioPlayer player, Duration initial, Duration end){
-  player.stop();
-  player!.seek(Duration.zero);
-  player!.setClip(start: initial , end: end);
+void stop (AudioPlayer player, Duration initial, Duration end) async {
+  await player.stop();
+  await player!.seek(Duration.zero);
+  await player!.setClip(start: initial , end: end);
 }
 
-void restart (AudioPlayer player, Duration initial, Duration end){
-  player!.stop();
-  player!.seek(Duration.zero);
-  player!.setClip(start: initial , end: end);
-  player.play();
+void restart (AudioPlayer player, Duration initial, Duration end) async {
+  await player!.stop();
+  await  player!.seek(Duration.zero);
+  await player!.setClip(start: initial , end: end);
+  await player.play();
+}
+Future<void> setup(AudioUI widget) async {
+  try {
+    await player.setUrl(url);
+    await player.seek(widget.initial!);
+    await player.setClip(start: widget.initial!, end: widget.end!);
+  } catch (e) {
+    throw (e);
+  }
 }
