c2c9042bc3974fe10a98564c118d4bceb4b26621
Sairaj <s.r.ghosarwadkar@gmail.com>
2024-03-02 13:46:01 +0530

    Removed pdf from assets and added to github pages

--- lib/screens/bhajan_lyrics_screen.dart
+++ lib/screens/bhajan_lyrics_screen.dart
@@ -14,7 +14,8 @@ class _BhajanLyricsScreenState extends State<BhajanLyricsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow,
-      body : SfPdfViewer.asset('assets/lyrics/book.pdf', pageLayoutMode: PdfPageLayoutMode.continuous, scrollDirection: PdfScrollDirection.horizontal)
+      body : SfPdfViewer.network("https://bhajanavali.github.io/book_compressed.pdf", pageLayoutMode: PdfPageLayoutMode.continuous, scrollDirection: PdfScrollDirection.horizontal)
+      //SfPdfViewer.asset('assets/lyrics/book.pdf', pageLayoutMode: PdfPageLayoutMode.continuous, scrollDirection: PdfScrollDirection.horizontal)
     );
   }
 }
