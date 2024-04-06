a2cd01866730fb4c7aae7a8b7c0951829bf0cae0
Sairaj <s.r.ghosarwadkar@gmail.com>
2024-04-06 20:18:01 +0530

    minor change

--- lib/components/bhajan_time_map.dart
+++ lib/components/bhajan_time_map.dart
@@ -45,7 +45,7 @@ List<Duration> bhajanEndDurations = [
   Duration(minutes: 3, seconds: 10),
   Duration(minutes: 2, seconds: 34),
   Duration(minutes: 3, seconds: 17),
-  Duration(minutes: 6, seconds: 17)
+  Duration(minutes: 6, seconds: 54)
 ];
 
 List<String> audioURL = [
